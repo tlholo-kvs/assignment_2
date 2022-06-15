febf259a1c93be84e31f86ad42e41805fa09a0c0
tlholo-kvs <tlholokgoele@hotmail.com>
2022-06-15 16:58:40 +0200

    Added validations for email addresses and passwords

--- lib/miscellaneous/validators.dart
+++ lib/miscellaneous/validators.dart
@@ -1,9 +1,35 @@
 String? validateEmail(String? value) {
-  return null;
+  String message = '';
+  RegExp emailPattern = RegExp(r'^[^@]+@[^@]+\.[^@]+');
+
+  if (!emailPattern.hasMatch(value!)) {
+    message = 'Email invalid! Please enter valid email, e.g example@site.com';
+  } else {
+    message = '';
+  }
+
+  return message;
 }
 
 String? validatePassword(String? value) {
-  return null;
+  String message = '';
+  //Rejex for password format
+  RegExp passwordPattern =
+      RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~])');
+
+  if (!passwordPattern.hasMatch(value!)) {
+    message = '''Password format incorrect! Password should contain at least one
+    uppercase letter, at least one lower case letter, one digit and at least 
+    one special character!
+     ''';
+  }
+  //testing seperately for the password length
+  if (value.length < 8) {
+    message = 'Please make sure your password is at least 8 characters long!';
+  } else {
+    message = '';
+  }
+  return message;
 }
 
 String? validateEmptyTitle(String? value) {
